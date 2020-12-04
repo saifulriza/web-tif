c2b82e4d10bd178c7653c2d73a99956ff7c30467
saifulriza123 <mail.saifulriza@gmail.com>
2020-12-04 17:04:46 +0700

    fix path

--- src/js/custom.js
+++ src/js/custom.js
@@ -50,21 +50,21 @@ function insertCarouselDesktop() {
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img data-src="carousel.jpg" class="lazy d-block w-100" alt="...">
+      <img data-src="/carousel.jpg" class="lazy d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img data-src="carousel.jpg" class="lazy d-block w-100" alt="...">
+      <img data-src="/carousel.jpg" class="lazy d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
       </div>
     </div>
     <div class="carousel-item">
-      <img data-src="carousel.jpg" class="lazy d-block w-100" alt="...">
+      <img data-src="/carousel.jpg" class="lazy d-block w-100" alt="...">
       <div class="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
