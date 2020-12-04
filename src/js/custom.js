e67e1b8038f17a4177a46d8ef660c792ecc4f357
saifulriza123 <mail.saifulriza@gmail.com>
2020-12-04 08:41:17 +0700

    fix error

--- src/js/custom.js
+++ src/js/custom.js
@@ -497,7 +497,7 @@ function insertSvgDesktop() {
 }
 
 if (screen && screen.width > 990) {
-  let rellax = new Rellax('.rellax');
+  if(document.getElementsByClassName('rellax'))  let rellax = new Rellax('.rellax');
   insertCarouselDesktop();
   insertWave();
   insertMenuDesktop();
