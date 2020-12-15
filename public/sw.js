a9efd17c59efb6a71404316270fd7189e53040db
saifulriza123 <mail.saifulriza@gmail.com>
2020-12-15 14:27:55 +0700

    add more sw

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,13 @@
 if ('function' === typeof importScripts) {
-  // importScripts(
-  //   'https://unpkg.com/workbox-sw@0.0.2/build/importScripts/workbox-sw.dev.v0.0.2.js',
-  // );
-  // importScripts(
-  //   'https://unpkg.com/workbox-runtime-caching@1.3.0/build/importScripts/workbox-runtime-caching.prod.v1.3.0.js',
-  // );
-  // importScripts(
-  //   'https://unpkg.com/workbox-routing@1.3.0/build/importScripts/workbox-sw.prod.v2.1.3.js',
-  // );
+  importScripts(
+    'https://unpkg.com/workbox-sw@0.0.2/build/importScripts/workbox-sw.dev.v0.0.2.js',
+  );
+  importScripts(
+    'https://unpkg.com/workbox-runtime-caching@1.3.0/build/importScripts/workbox-runtime-caching.prod.v1.3.0.js',
+  );
+  importScripts(
+    'https://unpkg.com/workbox-routing@1.3.0/build/importScripts/workbox-sw.prod.v2.1.3.js',
+  );
 
   importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.0.0/workbox-sw.js',
