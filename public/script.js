a306a1cd5b100e904bd222b8ebc2e4d793c67448
saifulriza123 <mail.saifulriza@gmail.com>
2020-12-15 14:13:20 +0700

    debug PWA

--- public/script.js
+++ public/script.js
@@ -9,13 +9,13 @@ window.onload = () => {
         var serviceWorker;
         if (registration.installing) {
           serviceWorker = registration.installing;
-          // console.log('SW installing');
+          console.log('SW installing');
         } else if (registration.waiting) {
           serviceWorker = registration.waiting;
-          // console.log('SW waiting');
+          console.log('SW waiting');
         } else if (registration.active) {
           serviceWorker = registration.active;
-          // console.log('SW active');
+          console.log('SW active');
         }
         if (serviceWorker) {
           // logState(serviceWorker.state);
@@ -27,6 +27,7 @@ window.onload = () => {
       .catch(function (error) {
         // Something went wrong during registration. The service-worker.js file
         // might be unavailable or contain a syntax error.
+        console.log(error);
       });
   } else {
     // The current browser doesn't support service workers.
